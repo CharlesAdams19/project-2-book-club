581f06202b45ef941056998b6ee2d34cfc1ec101
Charles Adams 19 <adamscra19@gmail.com>
2025-04-25 12:20:24 +0100

    updates for netfly

--- controllers/users.js
+++ controllers/users.js
@@ -6,21 +6,70 @@ import isLoggedIn from '../middleware/isLoggedIn.js'
 
 const router = express.Router()
 
-router.get('/profile', isLoggedIn,  async(req, res, next) => {
-    try{
-// find all reviews completed by reviewer
-const userReviews = await BookReview.find({reviewer:req.session.user._id})
-// get their book club info 
+router.get('/profile', isLoggedIn, async (req, res, next) => {
+    try {
+      const userId = req.session.user._id
+  
+      const user = await User.findById(userId).populate('bookClub')
+  
+      const userReviews = await BookReview.find({ reviewer: userId })
+  
+      const clubReviews = await BookReview.find({ 
+        bookClub: user.bookClub._id, 
+        reviewer: { $ne: userId } 
+      }).populate('reviewer')
+  
+      return res.render('users/profile.ejs', {
+        user,
+        userReviews,
+        clubReviews // 
+      })
+    } catch (error) {
+      return next(error)
+    }
+  })
 
-const user = await User.findById(req.session.user._id).populate('bookClub')
-// render the user page 
+router.post('/invite', isLoggedIn, async (req, res, next) => {
+    try {
+        const userId = req.session.user._id
+        const newClubId = req.body.clubId?.trim()
 
-return res.render('users/profile.ejs', {
-    user,
-    userReviews,
-})
-    } catch (error){
-return next (error)
+        const bookClub = await BookClub.findById(newClubId)
+        if (!bookClub) {
+            const user = await User.findById(userId).populate('bookClub')
+            const userReviews = await BookReview.find({ reviewer: userId })
+
+            return res.status(400).render('users/profile.ejs', {
+                user,
+                userReviews,
+                errorMessage: 'Invalid Book Club ID. Please try again.'
+            })
+        }
+
+        // Get the user and their current club
+        const user = await User.findById(userId)
+        const oldClubId = user.bookClub
+
+        // Move user to new club
+        user.bookClub = newClubId
+        await user.save()
+
+        // Add user to new club if not already a member
+        if (!bookClub.members.includes(userId)) {
+            bookClub.members.push(userId)
+            await bookClub.save()
+        }
+
+        // Optional: Remove from old club members
+        if (oldClubId && oldClubId.toString() !== newClubId) {
+            await BookClub.findByIdAndUpdate(oldClubId, {
+                $pull: { members: userId }
+            })
+        }
+
+        return res.redirect('/profile')
+    } catch (error) {
+        return next(error)
     }
 })
 
